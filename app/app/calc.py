367f71ef3e9a435fd134bf19363205d861d3e3b5
Sheikh Shahid ul Haq <shahidulhaq.pal@gmail.com>
2024-07-28 10:59:27 +0530

    added changes

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+
+"""
+def add(x, y):
+    """Add x and y and return the results"""
+    return x +y
+
+def subtract(x,y):
+    """Subtract x from y and return the result"""
+    return y - x
